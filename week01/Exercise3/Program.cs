172d2b0be14e41310a700bf70975524e1b0ebeac
joshuadevhub <elemide.j.dev@gmail.com>
2026-07-03 23:00:25 +0100

    feat: implement magic number guessing game

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -3,7 +3,37 @@ using System;
 class Program
 {
     static void Main(string[] args)
+  {
+    int numberOfGuess = 1;
+    Random randomGenerator = new Random();
+    int magicNumber = randomGenerator.Next(1, 101);
+
+    Console.WriteLine("Guess a number between 1 and 100");
+
+    Console.WriteLine();
+
+    Console.Write("What is the magic number: ");
+    string userInput = Console.ReadLine();
+    int userGuess = int.Parse(userInput);
+
+    while (userGuess != magicNumber)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+      if (userGuess < magicNumber)
+      {
+        Console.WriteLine("Higher");
+      }
+      else if (userGuess > magicNumber)
+      {
+        Console.WriteLine("Lower");
+      }
+
+      numberOfGuess++;
+      Console.Write("Try again. Enter a number: ");
+      userGuess = int.Parse(Console.ReadLine());
+    }
+    Console.WriteLine();
+
+    Console.WriteLine("Congratulations! You guessed the magic number.");
+    Console.WriteLine($"You guessed it in {numberOfGuess} attempts");
     }
 }
