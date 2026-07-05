17b3d725a8f5984d7a6832f7a98af424e6a503f6
joshuadevhub <elemide.j.dev@gmail.com>
2026-07-05 22:51:12 +0100

    feat: extract user prompts, calculations, and output into reusable methods

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,8 +2,41 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
-    }
+  static void Main(string[] args)
+  {
+    DisplayWelcome();
+    string username = PromptUserName();
+    int favoriteNumber = PromptUserNumber();
+    int squareNumber = SquareNumber(favoriteNumber);
+    DisplayResult(username, squareNumber);
+  }
+
+  static void DisplayWelcome()
+  {
+    Console.WriteLine("Welcome to the Program!");
+  }
+
+  static string PromptUserName()
+  {
+    Console.Write("Please enter your name: ");
+    string userName = Console.ReadLine();
+    return userName;
+  }
+
+  static int PromptUserNumber()
+  {
+    Console.Write("Please enter your favorite number: ");
+    int favoriteNumber = int.Parse(Console.ReadLine());
+    return favoriteNumber;
+  }
+
+  static int SquareNumber(int num)
+  {
+    return num * num;
+  }
+  
+  static void DisplayResult(string username, int squareNumbered)
+  {
+    Console.WriteLine($"Hello {username}, the square of your number is {squareNumbered}");
+  }
 }
